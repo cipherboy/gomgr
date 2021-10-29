e265c2e1627dd45dcab658d2cad33d3ff2ed2616
Alexander Scheel <alex.scheel@hashicorp.com>
2021-10-29 14:23:29 -0400

    [enable] Fix bugs related to ctrl+c, validate $PATH
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- src/commands/enable.bash
+++ src/commands/enable.bash
@@ -11,8 +11,8 @@ function ___gomgr_enable() {
     fi
 
     local identifier="go$version.$host-$arch"
-    local num_releases="$(ls -alh "$_gomgr_releases/$identifier".* | wc -l)"
-    if (( num_releases < 0 )); then
+    local num_releases="$(ls -alh "$_gomgr_releases/$identifier".* 2>/dev/null | wc -l)"
+    if (( num_releases < 1 )); then
         echo "This release is not present in cache; must fetch it first." 1>&2
         exit 1
     fi
@@ -35,10 +35,26 @@ function ___gomgr_enable() {
     if [ -e "$_gomgr_install_root/go" ]; then
         echo "Removing existing installation root: $_gomgr_install_root/go"
         sudo rm -rf "$_gomgr_install_root/go"
+        ret=$?
+        if (( ret != 0 )); then
+            echo "Unable to remove existing Go version" 1>&2
+            return $ret
+        fi
     fi
 
     echo "Installing new Go version $version"
     sudo tar -C "$_gomgr_install_root" -xf "$release_path"
+    ret=$?
+    if (( ret != 0 )); then
+        echo "Unable to unpack Go release" 1>&2
+        return $ret
+    fi
 
     "$_gomgr_install_root/go/bin/go" version
+
+    # Launch another shell to refresh $PATH cache...
+    if [ "$(bash -c 'command -v go')" != "$_gomgr_install_root/go/bin/go" ]; then
+        echo "Please add $_gomgr_install_root/go/bin to \$PATH ahead of /usr/bin." 1>&2
+        exit 1
+    fi
 }
